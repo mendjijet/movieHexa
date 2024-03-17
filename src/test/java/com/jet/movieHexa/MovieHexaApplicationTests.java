8c8de87146c67e09ffa0f12aa1cc75bac0bc0e36
mendjijet <mendjijet@gmail.com>
2024-03-17 18:30:55 +0100

    first commit init project movieHeagonale

---
+++ src/test/java/com/jet/movieHexa/MovieHexaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jet.movieHexa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieHexaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
